f33f35dbf8d4769c06250ef48a29ded639769d80
devcomfort <kimdonghyun026@gmail.com>
2023-05-15 06:41:43 +0900

    chore : 보일러플레이트 커밋

---
+++ 03-functions-and-classes/src/Presentation.svelte
@@ -0,0 +1,11 @@
+<script>
+  import Intro from "./slides/Intro.svelte";
+  import Additional from "./slides/Additional.svelte";
+  import Additional2 from "./slides/Additional2.svelte";
+  import SortAndSorted from "./slides/SortAndSorted.svelte";
+</script>
+
+<Intro />
+<Additional />
+<Additional2 />
+<SortAndSorted />
