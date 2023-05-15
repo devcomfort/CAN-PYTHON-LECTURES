15932f78c25722d188c36e09431198424f1e7100
devcomfort <kimdonghyun026@gmail.com>
2023-05-15 14:32:06 +0900

    fix : 데코레이터 추가

--- 03-functions-and-classes/src/slides/Classes.svelte
+++ 03-functions-and-classes/src/slides/Classes.svelte
@@ -1,5 +1,6 @@
 <script>
   import ClassDeclarationAndUse from "./ClassDeclarationAndUse.svelte";
+  import ClassDecorator from "./ClassDecorator.svelte";
   import ClassInheritence from "./ClassInheritence.svelte";
   import ClassVsInstance from "./ClassVSInstance.svelte";
 </script>
@@ -11,9 +12,11 @@
     <li>클래스 vs. 인스턴스</li>
     <li>클래스 선언과 사용</li>
     <li>클래스 상속; 오버라이딩, super()</li>
+    <li>데코레이터를 통한 특수한 메소드, 속성</li>
   </ol>
 </section>
 
 <ClassVsInstance />
 <ClassDeclarationAndUse />
 <ClassInheritence />
+<ClassDecorator />
