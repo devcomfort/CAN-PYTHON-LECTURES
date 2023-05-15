8ce2345ce1e15b59d88ea7e6e83e40bf5233a4d7
devcomfort <kimdonghyun026@gmail.com>
2023-05-15 14:31:59 +0900

    docs : 클래스에 사용되는 데코레이터 베이스코드 추가

---
+++ 03-functions-and-classes/src/slides/ClassDecorator.svelte
@@ -0,0 +1,13 @@
+<section>
+  <section>
+    <h3>데코레이터를 통한 특수한 메소드, 속성</h3>
+
+    <ul>
+      <li><code>@abstractmethod</code> 추상 메소드</li>
+      <li><code>@classmethod</code>클래스 메소드</li>
+      <li><code>@staticmethod</code>정적 메소드</li>
+      <li><code>@property</code> getter 데코레이터</li>
+      <li><code>@any.setter</code> setter 데코레이터</li>
+    </ul>
+  </section>
+</section>
