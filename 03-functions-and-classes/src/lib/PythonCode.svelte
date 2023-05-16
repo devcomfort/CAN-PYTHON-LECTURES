060762d9484afefa6e66e667c511c43f2ad8db5c
devcomfort <kimdonghyun026@gmail.com>
2023-05-17 08:09:57 +0900

    feat : 파이썬 코드 컴포넌트 추가

---
+++ 03-functions-and-classes/src/lib/PythonCode.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  export let line = "";
+</script>
+
+<pre>
+  <code
+    data-trim
+    data-noescape
+    data-line-numbers={line}
+    class="language-python">
+    <slot />
+  </code>
+</pre>
