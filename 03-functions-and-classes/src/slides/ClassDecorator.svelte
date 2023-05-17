cb6e0bad540b7537b72da87ab7b643c26deb14e9
devcomfort <kimdonghyun026@gmail.com>
2023-05-17 16:12:15 +0900

    docs : 추상클래스, 정적클래스, 클래스메소드 추가

--- 03-functions-and-classes/src/slides/ClassDecorator.svelte
+++ 03-functions-and-classes/src/slides/ClassDecorator.svelte
@@ -87,9 +87,10 @@
       <code
         data-trim
         data-noescape
-        data-line-numbers="|9-10"
+        data-line-numbers="|10-11,15"
         class="language-python">
         {`
+         # 클래스 선언 코드
           class Apple:
             당도: float
             무게: float
@@ -100,17 +101,8 @@
 
             def price(self):
               return (당도 + 무게) * 100
-        `}
-      </code>
-    </pre>
 
-    <pre>
-      <code
-        data-trim
-        data-noescape
-        data-line-numbers="|2"
-        class="language-python">
-        {`
+          # 실행 코드
           MyApple = Apple(15, 10)
           print(MyApple.price())
         `}
@@ -127,9 +119,10 @@
       <code
         data-trim
         data-noescape
-        data-line-numbers="|9-11"
+        data-line-numbers="|10-12,16"
         class="language-python">
         {`
+        # 선언 코드
         class Apple:
           당도: float
           무게: float
@@ -141,19 +134,10 @@
           @property
           def price(self):
             return (self.당도 + self.무게) * 100
-        `}
-      </code>
-    </pre>
 
-    <pre>
-      <code
-        data-trim
-        data-noescape
-        data-line-numbers="|2"
-        class="language-python">
-        {`
-          MyApple = Apple(15, 10)
-          print(MyApple.price)
+        # 실행 코드
+        MyApple = Apple(15, 10)
+        print(MyApple.price)
         `}
       </code>
     </pre>
@@ -315,6 +299,182 @@
   <section>
     <h2>클래스 틀 만들기 - 추상 클래스</h2>
 
-    <!-- TODO: 추상 클래스 내용 넣기 -->
+    <p class="text-2xl">
+      추상 클래스는 상속해야만 사용할 수 있는 <strong>상속 전용 클래스</strong
+      >를 만드는 문법입니다
+    </p>
+  </section>
+
+  <section data-auto-animate="">
+    <h2>예시</h2>
+
+    <p class="text-2xl">원본 코드: 일반적인 <code>Student</code> 클래스</p>
+
+    <PythonCode line="|19-20">
+      {`
+      class Student:
+          name: str
+          phone_number: str
+          sex: str
+          student_id: str
+          department: str
+          university_name: str
+          GPA: float
+
+          def __init__(self, name: str, phone_number: str, sex: str, university_name: str,  student_id: str, department: str, GPA: float) -> None:
+              self.name = name
+              self.phone_number = phone_number
+              self.sex = sex
+              self.university_name = university_name
+              self.student_id = student_id
+              self.department = department
+              self.GPA = GPA
+
+          def is_appliable_to_SNU(self, limit: float) -> bool:
+              return self.GPA >= limit
+      `}
+    </PythonCode>
+  </section>
+
+  <section data-auto-animate="">
+    <h2>예시</h2>
+
+    <p class="text-2xl">
+      상속 추가한 코드: 학과별 구분을 위해 상속을 추가한 코드 <br />
+      + 서울대학교(SNU)에 가기 위한 학점이 되는지, 안 되는지 여부 출력하는 함수
+    </p>
+
+    <PythonCode line="|21-23">
+      {`
+      from abc import abstractmethod
+
+      class Student:
+          name: str
+          phone_number: str
+          sex: str
+          student_id: str
+          department: str
+          university_name: str
+          GPA: float
+
+          def __init__(self, name: str, phone_number: str, sex: str, university_name: str,  student_id: str, department: str, GPA: float) -> None:
+              self.name = name
+              self.phone_number = phone_number
+              self.sex = sex
+              self.university_name = university_name
+              self.student_id = student_id
+              self.department = department
+              self.GPA = GPA
+          
+          @abstractmethod
+          def is_appliable_to_SNU(self, limit: float) -> bool:
+              pass
+      `}
+    </PythonCode>
+  </section>
+
+  <section data-auto-animate="">
+    <h2>예시</h2>
+
+    <p class="text-2xl">상속 받는 코드: 소프트웨어학과를 기준으로 상속 받기</p>
+
+    <PythonCode>
+      {`
+      class SoftwareStudent(Student):
+          def __init__(self, name: str, phone_number: str, sex: str, university_name: str, student_id: str, department: str, GPA: float) -> None:
+              super().__init__(name, phone_number, sex, university_name, student_id, department, GPA)
+
+          def is_appliable_to_SNU(self, limit: float) -> bool:
+              return self.GPA >= limit
+      `}
+    </PythonCode>
+  </section>
+</section>
+
+<section>
+  <section>
+    <h3>함수를 하나로 묶어보기 - 정적 메소드</h3>
+
+    <p class="text-2xl">
+      예시 상황: 수학적인 함수 <code>max</code>와 <code>sum</code>,
+      <code>divmod</code>를 한 곳에 묶고 싶을 때
+    </p>
+
+    <PythonCode>
+      {`
+      # 이렇게 쓰고 싶을 때
+
+      print(max(1, 2, 3, 4, 5))  # 5
+      print(sum(1, 2, 3, 4, 5))  # 15
+      print(divmod(10, 7))  # (1, 3) | (몫, 나머지)
+      `}
+    </PythonCode>
+  </section>
+
+  <section>
+    <p class="text-2x">아래와 같이 짤 수 있습니다 (실습)</p>
+    <PythonCode>
+      {`
+    from typing import Tuple
+
+    class Math:
+        @staticmethod
+        def max(*args: Tuple[int]):
+            _result: int = args[0]
+            for v in args[1:]:
+                if _result < v:
+                    _result = v
+                    
+            return _result
+        
+        @staticmethod
+        def sum(*args: Tuple[int]):
+            _result: int = args[0]
+            for v in args[1:]:
+                _result = _result + v
+
+            return _result
+
+        @staticmethod
+        def divmod(a: int, b: int) -> Tuple[int, int]:
+            return (a // b, a % b)
+    `}
+    </PythonCode>
+  </section>
+</section>
+
+<section>
+  <section>
+    <h3>싱글톤 패턴(?) 구현을 위하여</h3>
+    <p class="text-4xl font-bold">- 클래식 메소드 -</p>
+  </section>
+
+  <section>
+    <p class="text-3xl">
+      예시 상황: 코드에 2개 이상 존재하면 안 되는 <code>Counter</code> 만들기
+    </p>
+
+    <PythonCode>
+      {`
+      # 선언 코드
+      class Counter:
+          count: int = 0
+          
+          @classmethod
+          def increment(self):
+              self.count = self.count + 1
+              return self.count
+
+      # 사용 코드
+      print(Counter.increment())  # 1
+      print(Counter.increment())  # 2
+      print(Counter.increment())  # 3
+      `}
+    </PythonCode>
+
+    <ul class="text-2xl">
+      <li class="fragment fade-up">별도의 선언 없이</li>
+      <li class="fragment fade-up">하나의 인스턴스에 즉시 접근할 수 있음</li>
+    </ul>
   </section>
 </section>
