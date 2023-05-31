336220269819c606f2ec9898211056d409533472
devcomfort <kimdonghyun026@gmail.com>
2023-05-31 17:49:25 +0900

    feat : 함수 선언 기초 업데이트

--- slides/04-reviews/src/slides/Functions.svelte
+++ slides/04-reviews/src/slides/Functions.svelte
@@ -1,13 +1,62 @@
+<script>
+  import PythonCode from "../lib/PythonCode.svelte";
+</script>
+
 <section>
   <section>
     <h2>함수</h2>
     <h3>(Functions)</h3>
+  </section>
 
-    <p class="text-xl">
-      수학에서의 함수를 본 따서, 프로그래밍에서 특정 기능이나 계산을 하도록 하는
-      자동 매크로 문법
-    </p>
+  <section data-auto-animate>
+    <h3>함수 선언 원리</h3>
 
-    <p class="text-xl" />
+    <div class="text-3xl">
+      <span>'변환할 함수'</span>
+      <br />
+      &darr;
+      <br />
+      <code>
+        함수 이름, 함수에 전달할 값, 계산 로직, 반환값 등 <br />
+        '함수 구조' 선언
+      </code>
+    </div>
+  </section>
+
+  <section data-auto-animate>
+    <h3>함수 선언 예시</h3>
+
+    <div class="text-3xl">
+      <span>$f(x) = x^2$</span>
+      <br />
+      &darr;
+      <br />
+      <PythonCode>
+        {`
+        def f(x: int) -> int:
+          return x ** 2
+      `}
+      </PythonCode>
+    </div>
+  </section>
+
+  <section>
+    <h3>함수 선언 순서</h3>
+
+    <div class="grid grid-cols-2">
+      <ol class="text-3xl">
+        <li class="fragment fade-up">함수 이름 지정</li>
+        <li class="fragment fade-up">함수에 전달할 값 지정</li>
+        <li class="fragment fade-up">함수의 계산 로직 정의</li>
+        <li class="fragment fade-up">함수 반환값 지정</li>
+      </ol>
+      <PythonCode>
+        {`
+        def f(x: int) -> int:  # 1, 2
+          _return: int = x ** 2  # 3
+          return _return  # 4
+        `}
+      </PythonCode>
+    </div>
   </section>
 </section>
